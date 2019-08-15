95a61e8a6bffc38aea24cd220e2783ca2b7af51e
yuhongbo <1359320936@qq.com>
2019-08-15 16:54:13 +0800

    init

--- src/main/java/ywm/oms/OmsApplication.java
+++ src/main/java/ywm/oms/OmsApplication.java
@@ -1,7 +1,14 @@
 package ywm.oms;
 
+import com.wolf.Wolf;
+
 /**
  * Created by Herbert Yu on 2019-08-14 17:41
  */
+@Wolf.BootApplication
 public class OmsApplication {
+
+    public static void main(String[] args) {
+        Wolf.run(OmsApplication.class, args);
+    }
 }
