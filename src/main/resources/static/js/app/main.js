a82d018c306c2c115604e0e1362a9985893a91f6
yuhongbo <1359320936@qq.com>
2020-03-12 16:25:50 +0800

    deploy

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -160,6 +160,10 @@ const NS = "YWM";
                 return fmt;
             },
 
+            parseDate: function (str) {
+                return new Date(str.replace(/-/g, "/"));
+            },
+
             writeScript: function (url) {
                 document.write("<scr" + "ipt type='text/javascript' src=" + url + "></sc" + "ript>");
             },
@@ -295,6 +299,106 @@ const NS = "YWM";
                     hash = hash & hash;
                 }
                 return hash;
+            },
+
+            /**
+             * 对电话和姓名身份证等字符串 进行部分隐藏处理
+             * @param str 字符串
+             * @param frontLen 前面保留位数
+             * @param endLen 后面保留位数
+             * @returns {string}
+             */
+            replaceWithXing: function (str, frontLen, endLen) {
+                var len = str.length - frontLen - endLen;
+                var xing = '';
+                for (var i = 0; i < len; i++) {
+                    xing += '*';
+                }
+                return str.substring(0, frontLen) + xing + str.substring(str.length - endLen);
+            },
+            replaceMobileWithXing: function (str) {
+                return this.replaceWithXing(str, 3, 4);
+            },
+            replaceIdcardWithXing: function (str) {
+                return this.replaceWithXing(str, 6, 4);
+            },
+
+            IdcardValid: function (code, strict) {
+                let city = {
+                    11: "北京",
+                    12: "天津",
+                    13: "河北",
+                    14: "山西",
+                    15: "内蒙古",
+                    21: "辽宁",
+                    22: "吉林",
+                    23: "黑龙江 ",
+                    31: "上海",
+                    32: "江苏",
+                    33: "浙江",
+                    34: "安徽",
+                    35: "福建",
+                    36: "江西",
+                    37: "山东",
+                    41: "河南",
+                    42: "湖北 ",
+                    43: "湖南",
+                    44: "广东",
+                    45: "广西",
+                    46: "海南",
+                    50: "重庆",
+                    51: "四川",
+                    52: "贵州",
+                    53: "云南",
+                    54: "西藏 ",
+                    61: "陕西",
+                    62: "甘肃",
+                    63: "青海",
+                    64: "宁夏",
+                    65: "新疆",
+                    71: "台湾",
+                    81: "香港",
+                    82: "澳门",
+                    91: "国外 "
+                };
+                let tip = "success";
+                let pass = true;
+
+                if (!code || !/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i.test(code)) {
+                    tip = "身份证号码[格式]错误";
+                    pass = false;
+                }
+
+                if (strict) {
+                    if (!city[code.substr(0, 2)]) {
+                        tip = "身份证号码[地址编码]错误";
+                        pass = false;
+                    } else {
+                        //18位身份证需要验证最后一位校验位
+                        if (code.length == 18) {
+                            code = code.split('');
+                            //∑(ai×Wi)(mod 11)
+                            //加权因子
+                            var factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+                            //校验位
+                            var parity = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2];
+                            var sum = 0;
+                            var ai = 0;
+                            var wi = 0;
+                            for (var i = 0; i < 17; i++) {
+                                ai = code[i];
+                                wi = factor[i];
+                                sum += ai * wi;
+                            }
+                            var last = parity[sum % 11];
+                            if (parity[sum % 11] != code[17]) {
+                                tip = "身份证号码[校验位]错误";
+                                pass = false;
+                            }
+                        }
+                    }
+                }
+                return {pass: pass, tip: tip};
             },
 
             /**
