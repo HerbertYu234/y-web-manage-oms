ab691a05d1727464706d1c39d0e25137c568fd47
yuhongbo <1359320936@qq.com>
2020-10-30 15:35:56 +0800

    ywm

--- src/main/java/ywm/oms/OmsCloudlet.java
+++ src/main/java/ywm/oms/OmsCloudlet.java
@@ -2,12 +2,19 @@ package ywm.oms;
 
 import com.wolf.Wolf;
 import com.wolf.cloud.WolfCloudlet;
+import com.wolf.security.config.ShiroFilterBuilder;
+import com.wolf.security.config.WolfSecurityAutoConfiguration;
+import com.wolf.security.config.WolfSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import ywm.library.shared.security.SecurityService;
+import ywm.library.shared.security.SecurityUserFilter;
 import ywm.oms.websocket.HandShakeInterceptor;
 import ywm.oms.websocket.WolfWebSocketHandler;
 
@@ -15,9 +22,12 @@ import ywm.oms.websocket.WolfWebSocketHandler;
  * Created by Herbert Yu on 2019-08-14 17:41
  */
 @Wolf.BootApplication(name = "oms")
-@EnableFeignClients
+@EnableFeignClients() //basePackages = {"ywm"}
 @EnableWebSocket
-public class OmsCloudlet implements WolfCloudlet, WebSocketConfigurer {
+public class OmsCloudlet extends WolfSecurityConfigurerAdapter implements WolfCloudlet, WebSocketConfigurer {
+
+    @Autowired
+    private SecurityService securityService;
 
     public static void main(String[] args) {
         Wolf.run(OmsCloudlet.class, args);
@@ -33,5 +43,11 @@ public class OmsCloudlet implements WolfCloudlet, WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandlerRegist(), "/ws")
                 .addInterceptors(new HandShakeInterceptor()).setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureShiroFilter(ShiroFilterBuilder builder) {
+        builder.withUserFilter(new SecurityUserFilter(getApplicationContext(),
+                getSecurityProperties().getSessionKey()));
     }
 }
--- src/main/java/ywm/oms/controller/HomeController.java
+++ src/main/java/ywm/oms/controller/HomeController.java
@@ -4,8 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ywm.library.shared.model.ArticleType;
+import ywm.library.shared.model.ResEntity;
+import ywm.library.shared.model.UserDTO;
+import ywm.library.shared.remote.SharedUserService;
+import ywm.library.shared.security.SecurityService;
 import ywm.oms.service.remote.ArticleService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Herbert Yu on 2019-11-17 13:45
@@ -16,9 +24,16 @@ public class HomeController extends BaseController {
     @Autowired
     private ArticleService articleService;
 
+    @Autowired
+    private SharedUserService sharedUserService;
 
-    @GetMapping(value = {"", "/","/index"})
-    public String index(Model model) throws InterruptedException {
+    @Autowired
+    private SecurityService securityService;
+
+
+    @GetMapping(value = {"", "/", "/index"})
+    public String index(Model model) {
+        UserDTO admin = sharedUserService.findByUsername("admin");
         model.addAttribute("articleNum", articleService.articleCount(null));
         model.addAttribute("articleTypeNum", ArticleType.values().length);
         return "/index";
@@ -29,4 +44,13 @@ public class HomeController extends BaseController {
     private String plain(Model model) {
         return "/plain";
     }
+
+
+    @GetMapping("/logout")
+    @ResponseBody
+    public ResEntity logout(HttpServletRequest request, HttpServletResponse response) {
+        securityService.logout(request, response);
+        return ResEntity.success();
+    }
+
 }
